acc41107bfdcae53281db1a13bd65c2783c380a7
Bastien Matis <matis.bastien@outlook.fr>
2024-07-02 14:13:32 +0200

    project architecture

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,7 +2,7 @@ import { Router, Request, Response, NextFunction } from 'express';
 const router = Router();
 
 router.get('/', (req: Request, res: Response, next: NextFunction) => {
-  res.send('Hello World');
+    res.send('Hello World');
 });
 
 export default router;
