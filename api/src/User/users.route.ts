c55f75cca1662c3ca93de3520205dfe0312dac8c
Bastien Matis <81986949+BastienMatis@users.noreply.github.com>
2024-07-02 14:47:58 +0200

    project architecture (#2)
    * project architecture
    
    * project architecture

--- api/src/User/users.route.ts
+++ api/src/User/users.route.ts
@@ -2,7 +2,7 @@ import { Router, Request, Response, NextFunction } from 'express';
 const router = Router();
 
 router.get('/', (req: Request, res: Response, next: NextFunction) => {
-  res.send('User route');
+    res.send('User route');
 });
 
 export default router;
