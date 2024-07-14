b115201bad1343df1bebccdc554d839ea6c36d4b
Bastien Matis <matis.bastien@outlook.fr>
2024-07-14 15:15:04 +0200

    beginning home page

---
+++ app/src/screens/Menu/HomeScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { useNavigation } from '@react-navigation/native';
+import { Text } from "react-native";
+
+function HomeScreen(props) {
+    const navigation = useNavigation();
+    return (
+        <Text>ok</Text>
+    );
+}
+
+
+export default HomeScreen;
